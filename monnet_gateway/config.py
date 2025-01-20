a4eb5ee7ed5739e500c42a78a67adbe90bd7be5d
diegargon <diego@envigo.net>
2025-01-20 23:57:24 +0100

    split conf and impr ctx

--- monnet_gateway/config.py
+++ monnet_gateway/config.py
@@ -1,12 +1,12 @@
 """
-@copyright CC BY-NC-ND 4.0 @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
+@copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
 
-Monnet Gateway
+Monnet Ansible Gateway
 
 Config file
 """
 
-VERSION = "0.4"
+VERSION = "0.3"
 MINOR_VERSION = 9
 HOST = 'localhost'
 PORT = 65432
