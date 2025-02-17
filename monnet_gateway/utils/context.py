cf907f7a99ca6116727848739b360c36eeb41092
diegargon <diego@envigo.net>
2025-02-17 22:00:50 +0100

    .

--- monnet_gateway/utils/context.py
+++ monnet_gateway/utils/context.py
@@ -1,9 +1,8 @@
 """
-@copyright CC BY-NC-ND 4.0 @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
+@copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
 
 Monnet Gateway
 
-Class AppContext
 """
 
 class AppContext:
