aeefb6bb56c837a93e3010916d0ac85116ea05c5
diegargon <diego@envigo.net>
2025-03-21 21:19:23 +0100

    compose compatibility

--- monnet_gateway/config.py
+++ monnet_gateway/config.py
@@ -7,7 +7,7 @@ Config file
 """
 
 VERSION = "0.4"
-MINOR_VERSION = 31
+MINOR_VERSION = 33
 HOST = 'localhost'
 PORT = 65432
 PORT_TEST = 65433
--- monnet_gateway/install.bash
+++ monnet_gateway/install.bash
@@ -2,6 +2,7 @@
 
 APP_DIR="/opt/monnet-core/monnet_gateway"
 VENV_DIR="$APP_DIR/venv"
+SYSTEMD_DIR="/etc/systemd/system"
 
 if ! command -v pip &> /dev/null; then
     echo "Error: pip no está instalado."
@@ -9,38 +10,42 @@ if ! command -v pip &> /dev/null; then
 fi
 
 echo "Creating python/pip virtual environment $VENV_DIR..."
-python3 -m venv $VENV_DIR
+python3 -m venv "$VENV_DIR"
 
 
 echo "Installing dependencies..."
-source $VENV_DIR/bin/activate
+source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
 pip install -r $APP_DIR/requirements.txt
 deactivate
 
-echo "Settings permissions..."
-sudo chown -R root:root $APP_DIR
-sudo chmod -R 755 $APP_DIR
-
-SERVICE_FILE="/etc/systemd/system/monnet-gateway.service"
-echo "Configuring systemd $SERVICE_FILE..."
-cp ../files/monnet-gateway.service  $SERVICE_FILE
-chmod 644 $SERVICE_FILE
-
 ANSIBLE_CFG="/etc/ansible/ansible.cfg"
 echo "Configuring ansible: $ANSIBLE_CFG..."
 
-cat <<EOF | sudo tee $ANSIBLE_CFG > /dev/null
+# Configuración de Ansible
+mkdir -p "/etc/ansible"
+cat <<EOF > "$ANSIBLE_CFG"
 [defaults]
 stdout_callback=json
 callback_whitelist=json
 EOF
 
-echo "Reloading/Enabling systemd ..."
-sudo systemctl daemon-reload
-sudo systemctl enable monnet-gateway
-sudo systemctl start monnet-gateway
+# Allow continue using docker without systemd
 
-echo "Installation completed."
-echo "You can check the service status with: sudo systemctl status monnet-gateway"
-echo "You can check the logs with: sudo journalctl -u monnet-gateway"
+if [ -d "$SYSTEMD_DIR" ] && pidof systemd &> /dev/null; then
+    SERVICE_FILE="$SYSTEMD_DIR/monnet-gateway.service"
+    echo "Configuring systemd: $SERVICE_FILE..."
+
+    cp ../files/monnet-gateway.service "$SERVICE_FILE"
+    chmod 644 "$SERVICE_FILE"
+
+    echo "Reloading/Enabling systemd..."
+    systemctl daemon-reload
+    systemctl enable monnet-gateway
+    systemctl start monnet-gateway
+
+    echo "You can check the service status with: systemctl status monnet-gateway"
+    echo "You can check the logs with: journalctl -u monnet-gateway"
+else
+    echo "Systemd not detected or not running. Skipping systemd setup."
+fi
