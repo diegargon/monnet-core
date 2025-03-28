7c7da46d4df7103b6965607bb468663e1941d3d4
diegargon <diego@envigo.net>
2025-03-28 13:51:31 +0100

    up

--- monnet_agent/agent_globals.py
+++ monnet_agent/agent_globals.py
@@ -6,7 +6,7 @@ Global Vars
 Monnet Agent
 """
 
-AGENT_VERSION = "0.152"
+AGENT_VERSION = "0.153"
 # Config file
 CONFIG_FILE_PATH = "/etc/monnet/agent-config"
 DATASTORE_FILE_PATH = "/tmp/datastore.json"
