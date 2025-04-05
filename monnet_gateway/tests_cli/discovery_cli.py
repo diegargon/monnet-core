ce6d0fb1901c6b7c57e58fd50379611695adcea4
diegargon <diego@envigo.net>
2025-04-05 23:32:48 +0200

    comm

--- monnet_gateway/tests_cli/discovery_cli.py
+++ monnet_gateway/tests_cli/discovery_cli.py
@@ -1,7 +1,7 @@
 """
 @copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
 
-Monnet Gateway
+Monnet Gateway CLI TEST
 
 """
 
---
+++ monnet_gateway/tests_cli/known_cli.py
@@ -0,0 +1,6 @@
+"""
+@copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
+
+Monnet Gateway CLI TEST
+
+"""
