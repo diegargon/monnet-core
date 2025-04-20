9af355ceeb62165d9621361976d71d228036a990
diegargon <diego@envigo.net>
2025-04-20 13:08:00 +0200

    add checks

--- monnet_gateway/tasks/weekly_task.py
+++ monnet_gateway/tasks/weekly_task.py
@@ -2,6 +2,7 @@
 Weekly task to perform periodic updates, such as updating host details.
 """
 
+import ipaddress
 from monnet_gateway.services.hosts_service import HostService
 from monnet_gateway.networking.net_utils import get_hostname, get_mac, get_org_from_mac
 
@@ -21,42 +22,55 @@ class WeeklyTask:
         for host in hosts:
             updated = False
             hid = host.get("id")
+            ip  = host.get("ip")
             if not hid:
-                self.logger.error("Host ID is missing.")
+                self.logger.error("Host ID and IP is missing. WeeklyTask cannot continue.")
                 continue
-            # Update hostname if missing
-            if not host.get("hostname") and host.get("ip") is not None:
-                hostname = get_hostname(host.get("ip"))
-                if hostname is not None and isinstance(hostname, str):
+
+            try:
+                ipaddress.ip_address(ip)
+            except ValueError:
+                self.logger.error(f"Invalid IP address {ip} for host {hid}. Skipping...")
+                continue
+
+            # Check hostname missing/change
+            hostname = get_hostname(ip)
+            if hostname is not None and isinstance(hostname, str):
+                db_hostname = host.get("hostname", None)
+                if db_hostname is None and (db_hostname is None or db_hostname != hostname):
                     host["hostname"] = hostname
                     updated = True
 
-            # Update MAC if missing
-            if not host.get("mac") and host.get("ip") is not None:
-                mac = get_mac(host["ip"])
-                if mac is not None and isinstance(mac, str):
+            # Check MAC address missing/change
+            mac = get_mac(ip)
+            if mac is not None and isinstance(mac, str):
+                db_mac = host.get("mac", None)
+                if mac is not None and (db_mac is None or db_mac != mac):
                     host["mac"] = mac
                     updated = True
 
-            # Update MAC vendor if missing
-
+            # Update MAC vendor
             if mac is not None and isinstance(mac, str):
-                mac = host.get("mac")
                 # Asegurarse de que 'misc' sea un diccionario
                 if not isinstance(host.get("misc"), dict):
                     host["misc"] = {}
 
-                # Verificar si 'mac_vendor' está ausente
-                if not host.get("misc", {}).get("mac_vendor"):
-                    mac_vendor = get_org_from_mac(mac)
-                    if mac_vendor is not None and isinstance(mac_vendor, str):
+                mac_vendor = get_org_from_mac(mac)
+                if mac_vendor is not None and isinstance(mac_vendor, str):
+                    db_mac_vendor = host["misc"].get("mac_vendor", None)
+                    if mac_vendor is None and (db_mac_vendor is None or db_mac_vendor != mac_vendor):
                         host["misc"]["mac_vendor"] = mac_vendor
                         updated = True
 
             # Save updates to the database
             if updated:
-                #self.logger.info(f"Updating host {host}")
+                self.logger.debug(f"Updating host {host}")
                 # Update the host in the database
-                self.host_service.update(hid, host)
+                try:
+                    self.host_service.update(hid, host)
+                except Exception as e:
+                    self.logger.error(f"Failed to update host {hid} in weekly tasks: {e}")
+                    continue
+
 
         self.logger.info("WeeklyTask completed.")
