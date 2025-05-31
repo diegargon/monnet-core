80118b1cc3d4081d00d23cd4f7b2d978f2905a0f
diegargon <diego@envigo.net>
2025-05-31 13:28:40 +0200

    rename netuilts

--- monnet_agent/core/agent.py
+++ monnet_agent/core/agent.py
@@ -30,7 +30,7 @@ from monnet_agent.event_processor import EventProcessor
 from monnet_agent.handle_signals import handle_signal
 from monnet_agent.notifications import send_notification
 from monnet_agent.requests import send_request, validate_response
-from monnet_agent.netutils import send_wol, get_mac_from_ip, get_default_interface, get_own_mac
+from monnet_agent.agent_net_utils import send_wol, get_mac_from_ip, get_default_interface, get_own_mac
 
 class MonnetAgent:
     def __init__(self, ctx: AppContext):
